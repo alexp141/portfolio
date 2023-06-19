3e7739b086c7f36fd1feb577e5de9bd2604ad028
Alexander Pastor <apastor@umd.edu>
2023-06-18 20:29:14 -0400

    added basic routing to future project page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import About from './About/About'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProjectsPage from './Projects/ProjectsPage'
 
 function App() {
 
   return (
-    <>
-      <About />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<About />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ portfolio/src/Projects/ProjectsPage.jsx
@@ -0,0 +1,8 @@
+function ProjectsPage() {
+
+    return (
+        <h1>Projects page</h1>
+    )
+}
+
+export default ProjectsPage
